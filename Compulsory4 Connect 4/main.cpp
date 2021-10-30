b162f948a8e56b288311dfb9a03475f92b88fff6
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2021-10-30 15:06:02 +0200

    Added display of gameboard and selection bar.
     - Added drawBoard() which prints gameboard to terminal.
     - Added display() which prints gameboard with selection bar and allows choice of column.

--- Compulsory4 Connect 4/main.cpp
+++ Compulsory4 Connect 4/main.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include <vector>
 #include <conio.h>
-#include <windows.h>
 
 // using termcolor by Ihor Kalnytskyi for coloring text in terminal:
 #include "termcolor.hpp"  // https://github.com/ikalnytskyi/termcolor.git
@@ -12,12 +11,114 @@
 using std::cout; using std::cin; using std::string;
 using std::endl; using std::vector;
 
-void start_task(int);
+void menu(vector<vector<char>>&);
+void startTask(int, vector<vector<char>>&);
+void createBoard(vector<vector<char>>&, int, int);
+void drawBoard(vector<vector<char>>&);
+void display(vector<vector<char>>&);
 
-void menu() {
+int main() {
+	vector<vector<char>> board{};
+	createBoard(board, 6, 7);
+
+	menu(board);
+
+	return 0;
+}
+
+void startTask(int position, vector<vector<char>>& board) {
+	switch (position) {
+	case 0:
+		display(board);
+		break;
+	case 1:
+		break;
+	case 2:
+		exit(0);
+	default:
+		break;
+	}
+}
+
+void createBoard(vector<vector<char>>& board, int num_rows, int num_cols) {
+	for (size_t i = 0; i < num_rows; i++) {
+		board.push_back(vector<char>(num_cols, ' '));
+	}
+}
+
+void drawBoard(vector<vector<char>>& board) {
+	for (size_t i = 0; i <= board.size(); i++) {
+		cout << termcolor::bright_blue << " |";
+		for (size_t j = 0; j < 4.5*board.size(); j++) cout << "-";
+		cout << "| " << termcolor::reset << endl;
+		if (i == board.size()) break;
+
+		for (size_t j = 0; j < board[0].size(); j++) {
+			cout << termcolor::bright_blue << " | ";
+			switch (board[i][j]) {
+			case ' ':
+				cout << termcolor::reset;
+				break;
+			case 'o':
+				cout << termcolor::bright_yellow;
+				break;
+			case 'x':
+				cout << termcolor::bright_red;
+			default:
+				break;
+			}
+			cout << board[i][j];
+		}
+		cout << termcolor::bright_blue << " | " << termcolor::reset << endl;
+	}
+}
+
+void display(vector<vector<char>>& board) {
+	char input{};
+	int position{(int) board[0].size()/2};
+	vector<char> selection_bar(board[0].size(), ' ');
+	
+	while (true) {
+
+		selection_bar[position] = 'v';
+
+		for (int i = 0; i < selection_bar.size(); i++) {
+			if (i == position) cout << termcolor::bright_green;
+			cout << "   " << selection_bar[i];
+		}
+		cout << termcolor::reset << endl;
+
+		for (int i = 0; i < selection_bar.size(); i++) {
+			if (i == position) { cout << termcolor::bright_yellow << "   o"; }
+			else { cout << "   " << selection_bar[i]; }
+		}
+		cout << termcolor::reset << endl;
+
+		drawBoard(board);
+		
+		input = _getch();
+		system("cls");
+
+		switch (input) {
+		case 'a':
+			selection_bar[position--] = ' ';
+			break;
+		case 'd':
+			selection_bar[position++] = ' ';
+			break;
+		default:
+			break;
+		}
+
+		if (position < 0) { position = selection_bar.size() - 1; }
+		else if (position >= selection_bar.size()) { position = 0; }
+	}
+}
+
+void menu(vector<vector<char>>& board) {
 	int position{};
 	char input{};
-	vector<string> menu_options = { "Play against player.", "Play against AI.", "Quit to desktop."};
+	vector<string> menu_options = { "Play against player.", "Play against AI.", "Quit to desktop." };
 
 	while (true) {
 		system("cls");
@@ -41,7 +142,7 @@ void menu() {
 			break;
 		case '\r':  // selects current menu item on press of enter key.
 			system("cls");
-			start_task(position);
+			startTask(position, board);
 			system("pause");
 			break;
 		default:
@@ -54,22 +155,4 @@ void menu() {
 
 	}
 
-}
-
-int main() {
-	menu();
-	return 0;
-}
-
-void start_task(int position) {
-	switch (position) {
-	case 0:
-		break;
-	case 1:
-		break;
-	case 2:
-		exit(0);
-	default:
-		break;
-	}
 }
