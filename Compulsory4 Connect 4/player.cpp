10438a2d2bbd87e8a4270a994ab2df70946adad8
Anders Petershagen Åsbø <39097620+FunkMarvel@users.noreply.github.com>
2021-11-15 11:00:39 +0100

    Added comments to player-files.

--- Compulsory4 Connect 4/player.cpp
+++ Compulsory4 Connect 4/player.cpp
@@ -1,4 +1,7 @@
-#include "player.h"
+// Compulsory 3, Anders P. Asbo.
+// Contains Player class methods and related functions.
+
+#include "player.h"  // includes player decleration.
 #include <fstream>
 
 void Player::save(string filename) {
@@ -7,39 +10,44 @@ void Player::save(string filename) {
 	// Args:
 	//	filename -- string containing filename of file.
 
-	bool is_saved{ false }, found_line{ false };
+	bool is_saved{ false }, found_line{ false };  // bools for file parsing.
+
+	// opens file for both reading and writing:
 	std::fstream ofile(filename, std::ios::in | std::ios::out | std::ios::ate);
 	string line{};
-	int start{}, middle{}, line_pos{};
-	vector<string> lines{};
-	calcHighScore();
+	int start{}, middle{}, line_pos{};  // for finding current player's name in file.
+	vector<string> lines{};  // vector for storing all lines read from file.
+	calcHighScore();  // updates total score.
 	
 	
 	if (ofile.is_open()) { // saves data to end of file if file is open.
-		ofile.seekg(0);
-		while (getline(ofile, line)) {
+		ofile.seekg(0);  // makes sure to read from top of file.
+
+		while (getline(ofile, line)) { // loops through file and finds names:
 			start = line.find('{');
 			middle = line.find(':');
+			lines.push_back(line);  // saves current line in vector.
 
-			lines.push_back(line);
+			// if name in current line matches name of current player, save line number:
 			if (line.size() != 0 && line.substr(start + 1, middle - start - 1) == name) {
 				line_pos = lines.size() - 1;
 				found_line = true;
 			}
 		}
-		ofile.clear();
-		ofile.seekg(0);
+		ofile.clear();  // clears flags.
+		ofile.seekg(0);  // moves to top again.
+
 		for (size_t i = 0; i < lines.size(); i++) {
-			if (i != line_pos || !found_line) {
+			if (i != line_pos || !found_line) {  // writes stored lines back to file in same order:
 				ofile << lines[i] << endl;
 			}
-			else {
+			else {  // if line with current player is reached, write new score:
 				ofile << *this << endl;
 				is_saved = true;
 			}
 		}
 
-		if (!is_saved) ofile << *this << endl;
+		if (!is_saved) ofile << *this << endl;  // if current player was not written to file, append current player to bottom of file.
 		ofile.close();
 	}
 	else {
@@ -50,15 +58,20 @@ void Player::save(string filename) {
 }
 
 void Player::load(string filename) {
+	// Method for loading player score from file.
+	//
+	// Args:
+	//	filename -- string containing filename of file.
+
 	std::ifstream ifile(filename);
 
-	if (!(ifile.is_open())) {
+	if (!(ifile.is_open())) { // creates new savefile if opening failed:
 		std::fstream ofile(filename, std::ios::out);
 		ofile.close();
 		ifile = std::ifstream(filename);
 	}
 
-	if (ifile.is_open()) {
+	if (ifile.is_open()) {  // seeks through file:
 		string line{};
 		int start{}, middle{}, end{};
 		while (getline(ifile, line)) {
@@ -66,12 +79,13 @@ void Player::load(string filename) {
 			middle = line.find(':');
 			end = line.find('}');
 
+			// if current entry in file has same name as current player, load score:
 			if (line.size() != 0 && line.substr(start + 1, middle - start - 1) == name) {
 				old_score = stoi(line.substr(middle + 1, end - middle - 1));
 				break;
 			}
 		}
-		ifile.close();
+		ifile.close();  // old_score remains 0 if no saved score is found.
 	}
 	else {
 		// exits on failure to open savefile:
@@ -81,27 +95,52 @@ void Player::load(string filename) {
 }
 
 std::ostream& operator<<(std::ostream& os, Player player) {
+	// insertion operator overload. Returns refrence to ostream after inserting string {name:score}
+	//
+	// Args:
+	//		os -- ostream refrence to insert into.
+	//		player -- Player-object to retrieve name and score from.
+
 	os << "{" << player.name << ":" << player.old_score << "}";
 	return os;
 }
 
 
 std::ostream& operator<<(std::ostream& os, std::unique_ptr<Player>& player) {
-	os << *player;
+	// insertion operator overload. Returns refrence to ostream after inserting string {name:score}
+	//
+	// Args:
+	//		os -- ostream refrence to insert into.
+	//		player -- Player-object to retrieve name and score from.
+
+	os << *player;  // uses previous overload.
 	return os;
 }
 
 void createPlayer(int player_number, std::unique_ptr<Player>& player) {
+	// Function creating player from user-provided name. Player object is stored in
+	// memory-location pointed too by player-argument.
+	//
+	// Args:
+	//		player_number -- int corresponding to wether creating player 1 or 2.
+	//		player -- unique_ptr that points to memory-location where created player will be stored.
+
 	string name{};
-	char mark{'x'};
+	char mark{'x'}; // sets correct game-piece:
 	if (player_number % 2) mark = 'o';
 
+	// asks for name, no spaces allowed:
 	cout << " Enter player " << player_number << " name (no spaces): ";
 	cin >> name;
-	player = std::make_unique<Player>(Player(name, mark));
+	player = std::make_unique<Player>(Player(name, mark));  // stores new player object.
 }
 
 vector<Player> loadPlayers(string filename) {
+	// Function for loading all players from file.
+	//
+	// Args:
+	//	filename -- string containing filename of file.
+
 	vector<Player> players{};
 	std::ifstream ifile(filename);
 
@@ -109,6 +148,7 @@ vector<Player> loadPlayers(string filename) {
 		string line{};
 		int start{}, middle{}, end{};
 
+		// loads name and score from each line:
 		while (getline(ifile, line)) {
 			string name{};
 			int score{};
@@ -121,17 +161,18 @@ vector<Player> loadPlayers(string filename) {
 				name = line.substr(start + 1, middle - start - 1);
 				score = stoi(line.substr(middle + 1, end - middle - 1));
 
-				players.push_back(Player(name, score));
+				players.push_back(Player(name, score));  // saves player to return-vector.
 			}
 		}
 		ifile.close();
 
-		int ai_index{ -1 };
+		int ai_index{ -1 };  // finds AI entry in return-vector and erases it.
 		for (size_t i = 0; i < players.size(); i++) {
-			if (players[i].getName() == "A I") ai_index = i;
+			if (players[i].getName() == "A I") ai_index = i;  // Only AI has space in name, since users can't input name with spaces.
 		}
-
 		if (ai_index >= 0) players.erase(players.begin() + ai_index);
+
+		// uses operator< overload and std::sort to sort players in return-vector from lowest score to highest core.
 		std::sort(players.begin(), players.end());
 		return players;
 	}
--- Compulsory4 Connect 4/player.h
+++ Compulsory4 Connect 4/player.h
@@ -1,47 +1,62 @@
 #pragma once
+// Compulsory 3, Anders P. Asbo.
+// Top-level header. Contains Player class and related functions.
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 
+// common std-functions:
 using std::cout; using std::cin; using std::string;
 using std::endl; using std::vector;
 
-class Player {
+class Player {  // class for player storing name, character for game piece, total score and scor of current session.
 public:
-	Player() { name = "AI"; mark = 'x'; }
-	Player(string _name, char _mark) { name = _name; mark = _mark; }
-	Player(string _name, int _score) { name = _name; old_score = _score; }
+	// constructors:
+	Player() { name = "A I"; mark = 'x'; }  // Default constructor makes AI player.
+	Player(string _name, char _mark) { name = _name; mark = _mark; }  // Construct with provided name and mark.
+	Player(string _name, int _score) { name = _name; old_score = _score; }  // Construct with provided name and high-score.
+
+	// destructor:
 	~Player() {}
 
+	// functions for retrieving and setting name:
 	inline string getName() { return name; }
 	inline void setName(string _name) { name = _name; }
 
+	// functions for retrieving and setting game-piece character:
 	inline char getMark() { return mark; }
 	inline void setMark(char _mark) { mark = _mark; }
 
+	// functions for retrieving and incrementing score:
 	inline unsigned int getScore() { return current_score; }
 	inline unsigned int getHighScore() { return old_score + current_score; }
 	inline void incrementScore() { current_score++; }
 
+	// functions for saving and loading name and score to and from file:
 	void save(string filename);
 	void load(string filename);
 
-	friend std::ostream& operator<<(std::ostream& os, Player player);
-	friend std::ostream& operator<<(std::ostream& os, std::unique_ptr<Player>& player);
-	friend bool operator<(Player player1, Player player2) { return player1.getHighScore() < player2.getHighScore(); }
+	// operator overloads:
+	friend std::ostream& operator<<(std::ostream& os, Player player);  // insertion operator used when saving to file
+	friend std::ostream& operator<<(std::ostream& os, std::unique_ptr<Player>& player);  // insertion operator used when saving to file
+	friend bool operator<(Player player1, Player player2) { return player1.getHighScore() < player2.getHighScore(); }  // < operator allows using std::sort on vector<Player>() object.
 
-	inline const static string getFilename() { return "savefile.txt"; }
+	inline const static string getFilename() { return "savefile.txt"; }  // static member for retrieving default filename.
 
 private:
-	char mark;
+	// private variables:
+	char mark;  // game-piece
 	string name;
-	unsigned int current_score{};
-	unsigned int old_score{};
+	unsigned int current_score{};  // session score.
+	unsigned int old_score{};  // score loaded from file.
 
+	// function calculating new total score for saving to file:
 	inline void calcHighScore() { old_score += current_score; }
 
 };
 
+// related stand-alone functions.
 void createPlayer(int, std::unique_ptr<Player>&);
 vector<Player> loadPlayers(string filename);
